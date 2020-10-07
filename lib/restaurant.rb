a9cfd83ee841e0b6ed2c80fc5d7ec7e30256ad8c
Grayson Myers <graymyers10@gmail.com>
2020-10-07 13:44:14 -0600

    Add announce_closing_time method to restaurant class

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -30,4 +30,11 @@ class Restaurant
     @opening_time.split(":")[0].to_i < 12
   end
 
+  def announce_closing_time(hours_open)
+    hour_open = @opening_time.split(":")[0].to_i
+    hour_open += hours_open
+    adjusted_time = hour_open - (hour_open > 12 ? 12 : 0)
+    "#{@name} will be closing at #{adjusted_time}:#{@opening_time.split(":")[1]}#{hour_open > 12 ? "PM" : "AM"}"
+  end
+
 end
